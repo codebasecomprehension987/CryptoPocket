1940a424e0da6d1f21b98aeda2663c343377bb78
codebasecomprehension987 <nonessentialtrying432@gmail.com>
2026-03-20 08:53:20 +0530

    feat: add Python arena wrapper with Rust fallback

---
+++ src/cryptopocket/arena/init.py
@@ -0,0 +1,8 @@
+from cryptopocket.arena import FrameArena, FrameHandle, create_arena, RUST_ARENA_AVAILABLE
+
+__all__ = [
+    "FrameArena",
+    "FrameHandle",
+    "create_arena",
+    "RUST_ARENA_AVAILABLE",
+]
