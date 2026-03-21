098930fc94f732cd1ba8db169c3ae06d3bed5410
codebasecomprehension987 <nonessentialtrying432@gmail.com>
2026-03-21 13:40:26 +0530

    feat: add utils package init

---
+++ src/cryptopocket/utils/init.py
@@ -0,0 +1,15 @@
+from cryptopocket.utils.geometry import (
+    rotation_6d_to_matrix,
+    gram_schmidt_se3,
+    se3_inverse,
+    relative_frames,
+    frames_to_coords,
+)
+
+__all__ = [
+    "rotation_6d_to_matrix",
+    "gram_schmidt_se3",
+    "se3_inverse",
+    "relative_frames",
+    "frames_to_coords",
+]
