71cf5b04955b180d8022a7ec3cfd6044d4d569c0
codebasecomprehension987 <nonessentialtrying432@gmail.com>
2026-03-22 07:56:47 +0530

    feat: add cffi package init

---
+++ src/cryptopocket/cffi/init.py
@@ -0,0 +1,15 @@
+from cryptopocket.cffi.openfold_hook import (
+    PocketProbeNetwork,
+    OpenFold3HookManager,
+    OPENFOLD3_AVAILABLE,
+    CUPY_AVAILABLE,
+    install_openfold3_hook,
+)
+
+__all__ = [
+    "PocketProbeNetwork",
+    "OpenFold3HookManager",
+    "OPENFOLD3_AVAILABLE",
+    "CUPY_AVAILABLE",
+    "install_openfold3_hook",
+]
